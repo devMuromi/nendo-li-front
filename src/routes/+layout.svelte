a61340eabdfd0a31b6a167fc4a3baed4ff23b478
devMuromi <devmuromi@gmail.com>
2022-10-29 20:02:08 +0900

    several changes with navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,50 +1,70 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/nendoroids">Nendoroids</a>
-	<!-- <a href="/my">My Page</a>
-    <a href="/settings">Settings</a> -->
-</nav>
-<div id="main">
-	<slot />
+<div class="root">
+	<nav class="nav">
+		<a class="nav__item" href="/">Home</a>
+		<a class="nav__item" href="/nendoroids">Nendoroids</a>
+		<a class="nav__item" href="/nendoroids">testestsetsetstNs</a>
+		<a class="nav__item" href="/my">My Page</a>
+		<a class="nav__item" href="/settings">Settings</a>
+	</nav>
+	<div class="header">Nendoroids</div>
+	<div class="main">
+		<slot />
+	</div>
 </div>
 
 <style>
+	.root {
+		width: 100%;
+	}
 	:global(body) {
 		margin: 0px;
 		font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
 			'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
 			'Segoe UI Symbol', 'Noto Color Emoji';
 	}
-	nav {
+	.nav {
 		position: fixed;
-		display: inline-block;
-		width: minmax(80px, 8%);
-
+		top: 0;
+		left: 0;
+		display: flex;
+		flex-direction: column;
+		width: 80px;
 		height: 100%;
-		margin: 0;
+
 		background-color: whitesmoke;
 	}
-	a:hover {
+	.nav__item:hover {
 		background-color: gainsboro;
 	}
-	a {
-		display: block;
-		height: 80px;
+	.nav__item {
+		flex-basis: 80px;
 		line-height: 80px;
+
 		text-align: center;
 		border: 1px solid #ddd;
 		font-weight: 600;
 		color: black;
 		font-size: 0.875rem; /* 14px */
 		text-decoration-line: none;
+
+		overflow: hidden;
 	}
-	#main {
-		display: inline-block;
-		align-items: right;
-		height: 100vh;
-		width: 92%;
-		margin: 0;
+	.header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
 		margin-left: 80px;
-		padding: 10px;
+		height: 60px;
+		line-height: 60px;
+		padding-left: 10px;
+		font-size: 24px;
+		background-color: tomato;
+	}
+	.main {
+		width: auto;
+		margin-left: 80px;
+		margin-top: 60px;
+		padding: 10px 0 0 10px;
 	}
 </style>
