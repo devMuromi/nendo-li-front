3b5afcf8446510439dc62ca9c9cf6b224d90c164
Muromi <devmuromi@gmail.com>
2022-12-30 22:55:24 +0900

    FEAT: changed layout style, add login page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,50 @@
 <script>
-	let header = 'Nendoroid';
-
 	let isLogin = false;
+	let navbarOpen = false;
+
+	function closeNavbar() {
+		navbarOpen = false;
+	}
 </script>
 
 <div class="root">
-	<nav class="nav">
-		<a class="nav__item" href="/">Home</a>
-		<a class="nav__item" href="/nendoroids">넨도로이드</a>
-		<a class="nav__item" href="/series">시리즈</a>
-		<a class="nav__item" href="/news">뉴스</a>
+	<div class="header">
+		<button
+			class="nav-toggle-button"
+			on:click={() => {
+				navbarOpen = !navbarOpen;
+			}}>///</button
+		>
+		<div class="header-title">Nendo-li</div>
 		{#if isLogin}
-			<a class="nav__item" href="/mypage">마이페이지</a>
-			<a class="nav__item" href="/logout">로그아웃</a>
+			<a class="header-login" href="/logout">로그아웃</a>
 		{:else}
-			<a class="nav__item" href="/login">로그인</a>
+			<a class="header-login" href="/login">로그인</a>
 		{/if}
-		<a class="nav__item" href="/my">마이페이지</a>
-	</nav>
-	<div class="header">{header}</div>
+
+		<!-- 오른쪽에 로그인 로그아웃 넣음 될듯 -->
+	</div>
+	{#if navbarOpen}
+		<nav class="nav">
+			<div class="nav__item"><a class="nav__text" href="/" on:click={closeNavbar}>Home</a></div>
+			<div class="nav__item">
+				<a class="nav__text" href="/nendoroids" on:click={closeNavbar}>넨도로이드</a>
+			</div>
+			<div class="nav__item">
+				<a class="nav__text" href="/series" on:click={closeNavbar}>시리즈</a>
+			</div>
+			<div class="nav__item"><a class="nav__text" href="/news" on:click={closeNavbar}>뉴스</a></div>
+			{#if isLogin}
+				<div class="nav__item">
+					<a class="nav__text" href="/mypage" on:click={closeNavbar}>마이페이지</a>
+				</div>
+				<div class="nav__item">
+					<a class="nav__text" href="/logout" on:click={closeNavbar}>로그아웃</a>
+				</div>
+			{:else}{/if}
+		</nav>
+	{/if}
+
 	<div class="main">
 		<slot />
 	</div>
@@ -27,6 +53,10 @@
 <style>
 	.root {
 		width: 100%;
+		height: 100%;
+		--color2: #1363df;
+		--color2dark: #0d4db5;
+		--color4: #dff6ff;
 	}
 	:global(body) {
 		margin: 0px;
@@ -34,49 +64,69 @@
 			'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
 			'Segoe UI Symbol', 'Noto Color Emoji';
 	}
+
+	.header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 40px;
+		line-height: 40px;
+		background-color: var(--color2);
+
+		font-size: 20px;
+		color: white;
+	}
+	.header-title {
+		display: inline-block;
+	}
+	.header-login {
+		display: inline-block;
+		float: right;
+		margin-right: 16px;
+
+		color: white;
+		text-decoration-line: none;
+	}
+	.nav-toggle-button {
+		display: inline-block;
+		background-color: var(--color2);
+		border: none;
+		height: 40px;
+	}
 	.nav {
 		position: fixed;
 		top: 0;
 		left: 0;
 		display: flex;
 		flex-direction: column;
-		width: 80px;
+		width: 192px;
 		height: 100%;
-		margin-top: 60px;
+		margin-top: 40px;
 
-		background-color: whitesmoke;
+		background-color: white;
+		border-right: 1px solid lightgray;
+	}
+	.nav-button:hover {
+		background-color: var(--color2dark);
 	}
 	.nav__item:hover {
-		background-color: gainsboro;
+		background-color: var(--color4);
 	}
 	.nav__item {
-		flex-basis: 80px;
-		line-height: 80px;
-
-		text-align: center;
-		border: 1px solid #ddd;
-		font-weight: 600;
-		color: black;
-		font-size: 0.875rem; /* 14px */
-		text-decoration-line: none;
-
-		overflow: hidden;
+		flex-basis: 32px;
+		line-height: 32px;
 	}
-	.header {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 60px;
-		line-height: 60px;
-		padding-left: 10px;
-		font-size: 24px;
-		background-color: tomato;
+	.nav__text {
+		color: black;
+		font-size: 14px;
+		text-decoration-line: none;
+		padding-left: 12px;
+		overflow: hidden;
 	}
 	.main {
 		width: auto;
-		margin-left: 80px;
-		margin-top: 60px;
-		padding: 10px 0 0 10px;
+		margin-top: 40px;
+		padding: 16px;
 	}
 </style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	let isLogin = false;
+</script>
+
+{#if isLogin === false}
+	<div class="container">
+		<div class="">로그인</div>
+		<form class="">
+			<div class="">
+				<label class="form-label" for="email">이메일</label>
+				<input class="form-control" type="email" id="email" placeholder="이메일을 입력해주세요" />
+			</div>
+			<div class="">
+				<label class="form-label" for="password">비밀번호</label>
+				<input
+					class="form-control"
+					type="password"
+					id="password"
+					placeholder="비밀번호를 입력해주세요"
+				/>
+			</div>
+			<div class="">
+				<button class="btn btn-light" type="submit">로그인</button>
+			</div>
+		</form>
+	</div>
+{/if}
