5593c6cede275639338c7df0f2544a78a7e09768
Muromi <devmuromi@gmail.com>
2023-01-06 17:36:46 +0900

    Feat: add logout

---
+++ src/routes/logout/+page.js
@@ -0,0 +1,11 @@
+import { token, refreshToken } from '../../stores.js';
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').LayoutServerLoad} */
+export function load() {
+	if (token || refreshToken) {
+		token.set(null);
+		refreshToken.set(null);
+		throw redirect(307, '/');
+	}
+}
